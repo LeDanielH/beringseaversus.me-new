42a0b10acecad3e1b1cecd28d072ba6de667b38e
Daniel <daniel.hoksza@gmail.com>
2017-04-22 22:09:35 +0200

    vue integration - wip

--- devel/_assets/scripts/index.js
+++ devel/_assets/scripts/index.js
@@ -1,10 +1,13 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    $('.page .ui.sidebar')
-        .sidebar({
-            context: $('.page .bottom.segment'),
-            scrollLock: true,
-            returnScroll: true
-        })
-        .sidebar('attach events', '.page .menu .item');
-});
 
+
+import {$} from 'jquery'
+
+document.addEventListener('DOMContentLoaded', (event) => {
+	$('.page .ui.sidebar')
+		.sidebar({
+			context: $('.page .bottom.segment'),
+			scrollLock: true,
+			returnScroll: true
+		})
+		.sidebar('attach events', '.page .menu .item');
+});
